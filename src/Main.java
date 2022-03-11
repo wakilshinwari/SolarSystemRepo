e1fcdcca906232991028e0fc7f40a44154be54f2
ShinwariWakil <shinwariabdulw@gmail.com>
2022-03-11 11:26:04 -0500

    the basice has been complete

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,10 @@ public class Main {
 
     public static void main(String[] args) {
         SolarSystemPractice planets= new SolarSystemPractice();
-        
+
+planets.usn=1;
+planets.Moon=2;
+planets.Planets=3;
 
     }
 
